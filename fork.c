31ed36b04c682a2275321e96bf76e526bd72b4bc
Thomas <tchalifour91@gmail.com>
2022-03-24 13:17:54 -0400

    pipex

--- fork.c
+++ fork.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// int	main(void)
+// {
+
+// 	fork();
+// 	printf("Hello world\n");
+// 	return (0);
+// }
+
 int	main(void)
 {
-	// fork();
-	printf("Hello world!\n");
+	int	pid;
+	int	i;
+	int	n;
+
+	i = 0;
+	pid = fork();
+	if (pid == 0)
+		n = 1;
+	else
+		n = 6;
+	for (i = n; i < n + 5; i++)
+	{
+		printf("%d ", i);
+		fflush(stdout);
+	}
+	printf("\n");
 	return (0);
 }
