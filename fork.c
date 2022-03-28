92bd41f4b705f9ac038e741d806a74a1a28cde4f
Thomas <tchalifour91@gmail.com>
2022-03-28 07:46:28 -0400

    pipe notion

--- fork.c
+++ fork.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/wait.h>
+#include <errno.h>
+
+//Basic fork() exemple.
 // int	main(void)
 // {
 
@@ -9,25 +13,79 @@
 // 	return (0);
 // }
 
+
+//Basic wait() exemple.
+// int	main(void)
+// {
+// 	int	pid;
+// 	int	i;
+// 	int	n;
+
+// 	i = 0;
+// 	pid = fork();
+// 	if (pid == 0)
+// 		n = 1;
+// 	else
+// 		n = 6;
+// 	if (pid != 0)
+// 		wait(NULL);
+// 	else
+// 		printf("\n");
+// 	for (i = n; i < n + 5; i++)
+// 	{
+// 		printf("%d ", i);
+// 		fflush(stdout);
+// 	}
+
+// 	return (0);
+// }
+
+//wait() used for condition
+
+// int	main(void)
+// {
+// 	int	wait_result;
+// 	fork();
+// 	wait_result = wait(NULL);
+// 	if (wait_result == -1)
+// 		printf("The process %d dont have any children process\n", getppid());
+// 	else
+// 		printf("The child process %d have terminate is process\n", wait_result);
+// 	return (0);
+// }
+
+//Basic getpid() exemple.
+// int	main(void)
+// {
+// 	fork();
+// 	printf("%d\n", getpid());
+// 	return (0);
+// }
+
 int	main(void)
 {
-	int	pid;
-	int	i;
-	int	n;
+	int	id1;
+	int	id2;
 
-	i = 0;
-	pid = fork();
-	if (pid == 0)
-		n = 1;
-	else
-		n = 6;
-	if (pid != 0)
-		wait();
-	for (i = n; i < n + 5; i++)
+	id1 = fork();
+	id2 = fork();
+	if (id1 == 0)
 	{
-		printf("%d ", i);
-		fflush(stdout);
+		if (id2 == 0)
+			printf("Process <y>\n");
+		else
+			printf("Process <x>\n");
 	}
-	printf("\n");
+	else
+	{
+		if (id2 == 0)
+			printf("Process <z>\n");
+		else
+			printf("Process <original>\n");
+	}
+	while (wait(NULL) != -1 || errno != ECHILD)
+	{
+		printf("Waiting for child process to terminate\n");
+	}
 	return (0);
 }
