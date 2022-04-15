0ae2af5f95139a059f184fed63eec7ff57fbaa18
Thomas <tchalifour91@gmail.com>
2022-04-15 13:44:11 -0400

    another day

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tchalifo <tchalifo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tchalifo <tchalifo@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 09:27:40 by tchalifo          #+#    #+#             */
-/*   Updated: 2022/04/13 16:47:46 by tchalifo         ###   ########.fr       */
+/*   Updated: 2022/04/13 19:40:01 by tchalifo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,39 @@ static int	pipe_init(int *p_fd)
 	return (0);
 }
 
+char	*ft_strpatchr(const char *s, char *str, int delemiter)
+{
+	char	*start_of_str;
+	char	*ret_pattern;
+	int		i;
+	int		j;
 
+	i = 0;
+	j = 0;
+	start_of_str = str;
+	while (s[i] || s[i] == '=')
+	{
+		while (s[i] == str[j])
+		{
+			i++;
+			j++;
+		}
+		j = 0;
+		i++;
+
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (0);
+}
+
+char	*recup_the_bin_path(char **p_envp)
+{
+	char	*path;
+	ft_strchr()
+
+	while (ft_str_patternret() != -1)
+}
 int	main(int argc, char **argv, char **envp)
 {
 	(void) argc;
@@ -60,11 +92,9 @@ int	main(int argc, char **argv, char **envp)
 		return (2);
 	}
 
-	if (pipe_init(pipe_fd) == -1)
-	{
-		ft_printf("Pipe probleme\n");
+	if (pipe_init(pipe_fd) == -1) // pipe_fd[0] == read pipe_fd[1] == Write
 		return (3);
-	}
+
 	while (argc > 3)
 	{
 		pid = fork();
@@ -72,6 +102,7 @@ int	main(int argc, char **argv, char **envp)
 		{
 			//child process
 			ft_printf("Child process scope\n");
+			close(pipe_fd[0] )
 			if (execution(argv[2], envp) == -1)
 				return (4);
 			ft_printf("Child scope end\n");
