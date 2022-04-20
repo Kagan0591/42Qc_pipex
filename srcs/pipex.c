cf9a1d4c42b18f18d2b4bba9a1764cb86757f22f
Thomas <tchalifour91@gmail.com>
2022-04-19 21:41:05 -0400

    cmd work fabulously

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tchalifo <tchalifo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tchalifo <tchalifo@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 14:43:11 by tchalifo          #+#    #+#             */
-/*   Updated: 2022/04/18 21:14:51 by tchalifo         ###   ########.fr       */
+/*   Updated: 2022/04/19 21:39:33 by tchalifo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,22 +43,20 @@ char	*recup_the_bin_path(char *bin_name, char **p_envp)
 	complete_bin_path = NULL;
 	trimmed_path = path_env_var_to_str(p_envp);
 	splitted_path = ft_split(trimmed_path, ':');
+	free(trimmed_path);
 	while (splitted_path[i])
 	{
-	#error Verifier la splitted_path variable
-		complete_bin_path = ft_strjoin(splitted_path[i], bin_name);
-		//ft_printf("splitted path : %s\n", splitted_path[i]);
+		complete_bin_path = ft_strjoin(splitted_path[i], "/");
+		complete_bin_path = ft_strjoin(complete_bin_path, bin_name);
 		if (access(complete_bin_path, X_OK) != -1)
 		{
 			clear_char_tab(splitted_path);
-			free (complete_bin_path);
-			complete_bin_path = NULL;
 			return (complete_bin_path);
 		}
 		i++;
 	}
+	free (complete_bin_path);
 	clear_char_tab(splitted_path);
-	free(trimmed_path);
 	return (NULL);
 }
 
@@ -67,12 +65,11 @@ static int	execution(char *p_argv, char **p_envp)
 	char	**cmd;
 	char	*absolute_path;
 
-	absolute_path = recup_the_bin_path(p_argv, p_envp);
 	cmd = ft_split(p_argv, ' ');
-	ft_printf("%s\n", absolute_path);
+	absolute_path = recup_the_bin_path(cmd[0], p_envp);
+	ft_printf("absolute path = %s \n", absolute_path);
 	if (execve(absolute_path, cmd, p_envp) == -1)
 	{
-		ft_printf("TEST\n");
 		clear_char_tab(cmd);
 		free(absolute_path);
 		return (-1);
@@ -85,6 +82,7 @@ int	main(int argc, char **argv, char **envp)
 	int	outfile_fdnum;
 	int	i;
 	int	fork_pid;
+	int	pipefd[2];
 
 	outfile_fdnum = 0;
 
@@ -93,14 +91,13 @@ int	main(int argc, char **argv, char **envp)
 	if (outfile_fdnum == -1)
 		outfile_fdnum = open(argv[(argc - 1)], O_CREAT, S_IWUSR | S_IWGRP | S_IWOTH);
 	i = 2;
-	ft_printf("%d\n",argc);
-	while (i < argc)
+	while (i < (argc - 1))
 	{
 		fork_pid = fork();
 		if (fork_pid == 0)
 		{
 			if (execution(argv[i], envp) == -1)
-				return (1);
+				return (ft_printf("Exec probleme\n"));
 		}
 		else if (fork_pid != 0)
 			wait(0);
