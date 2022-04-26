04bed976a25ae9152fff01e4a1ab27cf85b36a5f
Thomas <tchalifour91@gmail.com>
2022-04-26 11:51:40 -0400

    Dinner time

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tchalifo <tchalifo@student.42quebec.com    +#+  +:+       +#+        */
+/*   By: tchalifo <tchalifo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 09:33:33 by tchalifo          #+#    #+#             */
-/*   Updated: 2022/04/26 10:13:22 by tchalifo         ###   ########.fr       */
+/*   Updated: 2022/04/26 11:51:20 by tchalifo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ static int	execution(char *p_argv, char **p_envp)
 
 	cmd = ft_split(p_argv, ' ');
 	absolute_path = recup_the_bin_path(cmd[0], p_envp);
-	// ft_printf("absolute path = %s \n", absolute_path);
+	perror("TESTEXEC\n");
 	if (execve(absolute_path, cmd, p_envp) == -1)
 	{
 		// ft_printf("TEST\n");
@@ -89,47 +89,47 @@ int	main(int argc, char **argv, char **envp)
 	filesfd[0] = open(argv[1], O_RDONLY);
 	filesfd[1] = open(argv[(argc - 1)], O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG | S_IRWXO);
 	ft_printf("FD0 = %d, FD1 = %d, \n", filesfd[0], filesfd[1]);
-	i = 0;
 	if (filesfd[0] == -1)
-		j = 3;
-	else
-		j = 2;
-	pipe(pipefd);
-	while (i < (argc - 3))
 	{
-		j = j + 1; //pour aligner les childs process sur les cmds en argument. Le premier process child aura la variable j = 2 pusiqu elle commence a 1 et obient 1 de plus au passage dans la boucle juste avant le fork.
-		if (i == 0) // Premier iteration le input doit etre rediriger vers le fichier intest ouvert plustot
-		{
+		i = 3;
+		j = 3;
+	}
+	else
+	{
+		i = 2;
+		j = 2;
+	}
+	pipe(pipefd);
+	while (i < (argc - 1))
+	{
+		if (i == 2) // Premier iteration le input doit etre rediriger vers le fichier intest ouvert plustot
 			dup2(filesfd[0], 0);
-		}
 		else
-		{
 			dup2(pipefd[READ_ENDPIPE], 0);
-		}
 		fork_pid = fork();
 		if (fork_pid == 0) //Child process
 		{
-			if (j == (argc - 2)) // Derniere iteration le WRITE_ENDPIPE doit etre rediriger vers le fichier outtest ouvert plustot
+			if (i == (argc - 2)) // Derniere iteration le WRITE_ENDPIPE doit etre rediriger vers le fichier outtest ouvert plustot
 			{
 				perror("TEST 1\n");
 				dup2(filesfd[1], 1);
-				close(pipefd[WRITE_ENDPIPE]);
 			}
 			else
 			{
 				perror("TEST 2\n");
 				dup2(pipefd[WRITE_ENDPIPE], 1);
 			}
-			if (execution(argv[j], envp) == -1)
+			if (execution(argv[i], envp) == -1)
 			{
 				perror("Exec probleme\n");
 				exit(2);
 			}
 		}
+		waitpid(-1, &waitpid_status, 0);
 		i++;
-		waitpid(fork_pid, &waitpid_status, 0);
-		perror("WHILE LOOP PASS : ");
+		perror("WHILE LOOP PASS : \n");
 	}
+	close(pipefd[WRITE_ENDPIPE]);
 	close(pipefd[READ_ENDPIPE]);
 	close(filesfd[0]);
 	close(filesfd[1]);
